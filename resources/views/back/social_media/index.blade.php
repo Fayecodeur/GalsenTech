801e6e88b4a21942a8bed46b3b3f1d27bfcf8c09
Fayecodeur <fayecodeur99@gmail.com>
2025-02-18 16:26:36 +0000

    ajout des liens pour modification et la suppression d'un reseau

--- resources/views/back/social_media/index.blade.php
+++ resources/views/back/social_media/index.blade.php
@@ -6,12 +6,10 @@
     <div class="row align-items-center">
         <div class="col">
             <div class="mt-5">
-                <h4 class="card-title float-left mt-2">Reseaux sociaux</h4>
-                <a
-                    href="{{route('social.create')}}"
-                    class="btn btn-primary float-right veiwbutton"
-                >Ajouter un reseau social</a
-                >
+                <h4 class="card-title float-left mt-2">Réseaux sociaux</h4>
+                <a href="{{ route('social.create') }}" class="btn btn-primary float-right veiwbutton">
+                    Ajouter un réseau social
+                </a>
             </div>
         </div>
     </div>
@@ -23,11 +21,11 @@
             <div class="card card-table">
                 <div class="card-body booking_card">
                     <div class="table-responsive">
-                        <table class="datatable table table-stripped table table-hover table-center mb-0">
+                        <table class="datatable table table-striped table-hover table-center mb-0">
                             <thead>
                             <tr>
                                 <th>ID Media</th>
-                                <th>Icon</th>
+                                <th>Icône</th>
                                 <th>Nom</th>
                                 <th>Lien</th>
                                 <th class="text-right">Actions</th>
@@ -35,17 +33,39 @@
                             </thead>
                             <tbody>
                             @foreach($socials as $social)
-                            <tr>
-                                <td>LIEN-{{$social->id}}</td>
-                                <td><i class="{{$social->icon}}"></i></td>
-                                <td>{{$social->name}}</td>
-                                <td><a href="">{{$social->link}}</a></td>
-                                <td class="text-right">
-                                    <div class="dropdown dropdown-action"> <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-ellipsis-v ellipse_color"></i></a>
-                                        <div class="dropdown-menu dropdown-menu-right"> <a class="dropdown-item" href="edit-categorie.html"><i class="fas fa-pencil-alt m-r-5"></i> Modifier</a> <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset"><i class="fas fa-trash-alt m-r-5"></i> Supprimer</a> </div>
-                                    </div>
-                                </td>
-                            </tr>
+                                <tr>
+                                    <td>LIEN-{{ $social->id }}</td>
+                                    <td><i class="{{ $social->icon }}"></i></td>
+                                    <td>{{ $social->name }}</td>
+                                    <td>
+                                        <a href="{{ $social->link }}" target="_blank">
+                                            {{ $social->link }}
+                                        </a>
+                                    </td>
+                                    <td class="text-right">
+                                        <div class="dropdown dropdown-action">
+                                            <a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+                                                <i class="fas fa-ellipsis-v ellipse_color"></i>
+                                            </a>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a class="dropdown-item" href="{{route('social.edit',$social)}}">
+                                                    <i class="fas fa-pencil-alt m-r-5"></i> Modifier
+                                                </a>
+                                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#delete_asset">
+                                                    <form method="POST" action="{{route('social.destroy', $social)}}">
+                                                        @csrf
+                                                        @method("DELETE")
+                                                        <button style="border: none; background: none; padding: 0; cursor: pointer;">
+                                                            <i class="fas fa-trash-alt m-r-5"></i> Supprimer
+                                                        </button>
+
+
+                                                    </form>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
                             @endforeach
                             </tbody>
                         </table>
