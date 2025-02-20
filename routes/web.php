e53b8e63e6f73fe6bbc8aa48696b82224440732d
Fayecodeur <fayecodeur99@gmail.com>
2025-02-20 16:47:45 +0000

    pratection des routes

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('back.dashboard');
-})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+})->middleware(['auth', 'verified', 'checkRole:admin,author'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -34,19 +34,19 @@ Route::middleware('auth')->group(function () {
 });
 
 //Partie categorie
-Route::resource('/category', CategoryController::class);
+Route::resource('/category', CategoryController::class)->middleware('admin');
 
 //Partie articles
 Route::resource('/article', ArticleController::class);
 
 //Partie author
-Route::resource('/author', UserController::class);
+Route::resource('/author', UserController::class)->middleware('admin');
 
 //Partie reseaux seciaux
-Route::resource('/social', SocialMediaController::class);
+Route::resource('/social', SocialMediaController::class)->middleware('admin');
 
 // Partie parametres
-Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
-Route::put('/settings/update', [SettingsController::class, 'update'])->name('settings.update');
+Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index')->middleware('admin');
+Route::put('/settings/update', [SettingsController::class, 'update'])->name('settings.update')->middleware('admin');
 
 require __DIR__ . '/auth.php';
