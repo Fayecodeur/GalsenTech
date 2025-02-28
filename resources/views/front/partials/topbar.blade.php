d8f1baab8adf9468ec01995f452bb74fd762c65f
Fayecodeur <fayecodeur99@gmail.com>
2025-02-28 15:42:11 +0000

    affichage de la date en francais

--- resources/views/front/partials/topbar.blade.php
+++ resources/views/front/partials/topbar.blade.php
@@ -4,9 +4,13 @@
             <nav class="navbar navbar-expand-sm bg-dark p-0">
                 <ul class="navbar-nav ml-n2">
                     <li class="nav-item border-right border-secondary">
-                        <a class="nav-link text-body small" href="#"
-                        >Lundi, Janvier 1, 2023</a
-                        >
+                        <a class="nav-link text-body small" href="#">
+                            @php
+                                \Carbon\Carbon::setLocale('fr');
+                                $time = \Carbon\Carbon::now();
+                            @endphp
+                            {{$time->dayName}}, {{$time->isoFormat('LL')}}
+                        </a>
                     </li>
 
                     <li class="nav-item">
@@ -18,36 +22,14 @@
         <div class="col-lg-3 text-right d-none d-md-block">
             <nav class="navbar navbar-expand-sm bg-dark p-0">
                 <ul class="navbar-nav ml-auto mr-n2">
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href="#"
-                        ><small class="fab fa-twitter"></small
-                            ></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href="#"
-                        ><small class="fab fa-facebook-f"></small
-                            ></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href="#"
-                        ><small class="fab fa-linkedin-in"></small
-                            ></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href="#"
-                        ><small class="fab fa-instagram"></small
-                            ></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href="#"
-                        ><small class="fab fa-google-plus-g"></small
-                            ></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-body" href="#"
-                        ><small class="fab fa-youtube"></small
-                            ></a>
-                    </li>
+                    @foreach($socials as $social)
+                        <li class="nav-item">
+                            <a class="nav-link text-body" href="{{$social->link}}"
+                            ><small class="{{$social->icon}}">
+                                </small
+                                ></a>
+                        </li>
+                    @endforeach
                 </ul>
             </nav>
         </div>
@@ -56,7 +38,7 @@
         <div class="col-lg-4">
             <a href="index.html" class="navbar-brand p-0 d-none d-lg-block">
                 <h1 class="m-0 display-4 text-uppercase text-info">
-                    Tech<span class="text-secondary font-weight-normal">News</span>
+                    Galsen<span class="text-secondary font-weight-normal">Tech</span>
                 </h1>
             </a>
         </div>
