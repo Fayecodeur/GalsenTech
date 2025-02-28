d1ebbfa242d0c0e71383caf809228c6b0edd5e39
Fayecodeur <fayecodeur99@gmail.com>
2025-02-28 15:40:44 +0000

    gestion de la langue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
+use App\Models\SocialMedia;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $socials = SocialMedia::orderBy("id", "desc")->get();
+        $categories = Category::where("isActive", 1)->orderBy('created_at', 'DESC')->get();
+
+        view()->share("socials", $socials);
+        view()->share("categories", $categories);
     }
 }
